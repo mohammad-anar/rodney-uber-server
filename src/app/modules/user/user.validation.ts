f39f519ac898db17b0022d910710d704948a5d80
Mohammad Anar <ai.anarul4232@gmail.com>
2026-01-26 00:43:07 +0600

    zod schema updated

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,20 +1,26 @@
 import { z } from 'zod';
 
 const createUserZodSchema = z.object({
+  body: z.object({
     name: z.string({ message: 'Name is required' }),
-    email: z.string({ message: 'Email is required' }),
+    email: z.email('Email is required or Invalid email'),
     phone: z.string().optional(),
-    password: z.string({ message: 'Password is required' }),
+    password: z
+      .string({ message: 'Password is required' })
+      .min(8, 'Password must be at least 8 characters'),
     address: z.string().optional(),
+  }),
 });
 
 const updateUserZodSchema = z.object({
-  name: z.string().optional(),
-  email: z.string().optional(),
-  phone: z.string().optional(),
-  password: z.string().optional(),
-  address: z.string().optional(),
-  image: z.string().optional(),
+  body: z.object({
+    name: z.string().optional(),
+    email: z.email().optional(),
+    phone: z.string().optional(),
+    password: z.string().min(8).optional(),
+    address: z.string().optional(),
+    image: z.string().optional(),
+  }),
 });
 
 export const UserValidation = {
