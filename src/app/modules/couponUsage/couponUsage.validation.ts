5cbdcdd5bcaa3aafae377bbda2cc2e25f488ac9b
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-02-15 02:09:26 +0600

    create couponUsage zod schema added

---
+++ src/app/modules/couponUsage/couponUsage.validation.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+import mongoose from 'mongoose';
+
+export const createCouponUsageSchema = z.object({
+  email: z
+    .string({
+      required_error: 'Email is required',
+    })
+    .email('Invalid email address')
+    .toLowerCase(),
+
+  coupon: z
+    .string({
+      required_error: 'Coupon ID is required',
+    })
+    .refine(val => mongoose.Types.ObjectId.isValid(val), {
+      message: 'Invalid coupon ID',
+    }),
+
+  video: z
+    .string({
+      required_error: 'Video ID is required',
+    })
+    .refine(val => mongoose.Types.ObjectId.isValid(val), {
+      message: 'Invalid video ID',
+    }),
+});
